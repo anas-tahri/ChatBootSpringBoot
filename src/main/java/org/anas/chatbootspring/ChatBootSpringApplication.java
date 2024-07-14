94c891c23602b593996f0a0cecdb148b8f315958
anastahri44 <anastahri44@gmail.com>
2024-07-14 02:06:36 +0100

    create chat boot project

---
+++ src/main/java/org/anas/chatbootspring/ChatBootSpringApplication.java
@@ -0,0 +1,13 @@
+package org.anas.chatbootspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBootSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBootSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/anas/chatbootspring/ChatBootSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anas.chatbootspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBootSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
